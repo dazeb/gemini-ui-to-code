e5c870ab9b7f35a00ce45afc82309bb340678427
Darren Bennett <dbennett2911@gmail.com>
2024-06-15 07:08:02 +0000

    c

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from PIL import Image
 import google.generativeai as genai
 
 # Configure the API key directly in the script
-API_KEY = 'YOUR KEY'
+API_KEY = 'AIzaSyDdggRxbnZKrMYffod-GROJPszGSeovsIA'
 genai.configure(api_key=API_KEY)
 
 # Generation configuration
